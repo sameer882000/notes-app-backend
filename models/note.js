dd909cb1cb92d2615312d61afd6dc3a7e2e1c836
Sameer Sinha <sameer.882000@gmail.com>
2024-10-15 18:37:52 +0530

    added auth in all apis

--- models/note.js
+++ models/note.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose');
 
-const NoteSchema = new mongoose.Schema({
-    title: String,
-    content: String,
-},
-{
-    timestamps: true
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    // This field will store the user ID
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Assuming you have a User model
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
     
-const Note = mongoose.model("Note", NoteSchema);
+const Note = mongoose.model("Note", noteSchema);
 module.exports = Note;
