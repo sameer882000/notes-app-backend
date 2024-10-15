941f52105381b5ec8f4c3861131a4cad44149c0d
Sameer Sinha <sameer.882000@gmail.com>
2024-10-15 13:14:29 +0530

    second commit

--- connection.js
+++ connection.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose');
 
 
 async function connect() {
-  mongoose.connect(
-  "mongodb+srv://sameer:12345677@cluster0.mongodb.net/notes-app?retryWrites=true&w=majority",
-  {
+  mongoose.connect("process.env.MONGODB_URI", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-      })
+  });
     // mongoose.connect(
     // "mongodb://localhost:27017/notes-app",
     // {
