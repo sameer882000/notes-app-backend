1ae8466d7ccce807e2811bc77b7d86dc37d19d59
Sameer Sinha <sameer.882000@gmail.com>
2024-10-15 16:42:23 +0530

    edit api added

--- routes/note.js
+++ routes/note.js
@@ -17,6 +17,14 @@ router.post('/', async (req, res) => {
   res.json(newNote);
 });
 
+// Update a note by ID
+router.put("/:id", async(req, res) => {
+  const { title, content } = req.body;
+ await  Note.findByIdAndUpdate(req.params.id, { title, content }, { new: true })
+    .then((note) => res.json(note))
+    .catch((err) => res.status(400).json({ error: err.message }));
+});
+
 router.delete('/:id', async (req, res) => {
   await Note.findByIdAndDelete(req.params.id);
   res.json({ message: 'Note deleted' });
