a3161ad1028dd8b15504f29595497de8a7687896
Sameer Sinha <sameer.882000@gmail.com>
2024-10-15 16:10:32 +0530

    fifth commit

--- models/note.js
+++ models/note.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const NoteSchema = new mongoose.Schema({
     title: String,
     content: String,
+},
+{
+    timestamps: true
 });
     
 const Note = mongoose.model("Note", NoteSchema);
